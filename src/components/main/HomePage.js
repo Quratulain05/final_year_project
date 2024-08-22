497d9162e3475eb8f23a52f1ca418756705df604
Quratulain <145496372+Quratulain05@users.noreply.github.com>
2024-08-22 15:55:25 +0500

    main page added

--- src/components/main/HomePage.js
+++ src/components/main/HomePage.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import { Button, Typography } from "@mui/material";
+import React from "react";
 
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className="flex flex-row px-20 items-center mainbgImg min-h-full">
+      <div className="bg-white h-1/2  " style={{ flex: 6 }}></div>
+      <div className=" flex justify-center" style={{ flex: 4 }}>
+        <Button className="button w-60 flex justify-center" sx={{ background: "white" }}>
+          <Typography
+            className="text-black"
+            style={{ fontWeight: "bolder", fontFamily: "Montserrat" }}
+          > Translate </Typography>
+        </Button>
+      </div>
+      <textarea className="bg-white h-1/2  " style={{ flex: 4 }}></textarea>
+    </div>
+  );
+};
